c31c1ef7d5acdc07e8729d105910b7235588e031
Chance Neff <crneff84@gmail.com>
2016-08-18 11:29:39 -0700

    update js to allow adding numbers, but no exceptions implemented

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,30 @@
 //Backend
 var total = 0;
 var converted = function(romanNumeral) {
-  for (var i = 0; i < romanNumeral.length; i++) {
-    if (romanNumeral[i] === romanNumeral[i - 1] )
-  }
-  var romanNumeralArray = romanNumeral.split("");
-  for (var i = 0; i < romanNumeralArray.length; i += 1) {
-    if (romanNumeralArray[i] === "I") {
-      romanNumeralArray.splice(i, 1, 1);
-      total += romanNumeralArray[i];
-    } else if (romanNumeralArray[i] === "V") {
-      romanNumeralArray.splice(i, 1, 5);
-      total += romanNumeralArray[i]
-    } else if (romanNumeralArray[i] === "X") {
-      romanNumeralArray.splice(i, 1, 10);
-      total += romanNumeralArray[i]
-    } else if (romanNumeralArray[i] === "L") {
-      romanNumeralArray.splice(i, 1, 50);
-      total += romanNumeralArray[i]
-    } else if (romanNumeralArray[i] === "C") {
-      romanNumeralArray.splice(i, 1, 100);
-      total += romanNumeralArray[i]
-    } else if (romanNumeralArray[i] === "D") {
-      romanNumeralArray.splice(i, 1, 500);
-      total += romanNumeralArray[i]
-    } else if (romanNumeralArray[i] === "M") {
-      romanNumeralArray.splice(i, 1, 1000);
-      total += romanNumeralArray[i]
-    }
+      var romanNumeralArray = romanNumeral.split("");
+      for (var i = 0; i < romanNumeralArray.length; i += 1) {
+        if (romanNumeralArray[i] === "I") {
+          romanNumeralArray.splice(i, 1, 1);
+          total += romanNumeralArray[i];
+        } else if (romanNumeralArray[i] === "V") {
+          romanNumeralArray.splice(i, 1, 5);
+          total += romanNumeralArray[i]
+        } else if (romanNumeralArray[i] === "X") {
+          romanNumeralArray.splice(i, 1, 10);
+          total += romanNumeralArray[i]
+        } else if (romanNumeralArray[i] === "L") {
+          romanNumeralArray.splice(i, 1, 50);
+          total += romanNumeralArray[i]
+        } else if (romanNumeralArray[i] === "C") {
+          romanNumeralArray.splice(i, 1, 100);
+          total += romanNumeralArray[i]
+        } else if (romanNumeralArray[i] === "D") {
+          romanNumeralArray.splice(i, 1, 500);
+          total += romanNumeralArray[i]
+        } else if (romanNumeralArray[i] === "M") {
+          romanNumeralArray.splice(i, 1, 1000);
+          total += romanNumeralArray[i]
+        }
   } return total;
 }
 
